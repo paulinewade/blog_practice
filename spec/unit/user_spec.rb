862bb1727bd565c2e33a308fcd3642efe8a71004
Wade <paulinewade@tamu.edu>
2023-09-04 22:46:51 -0500

    changed unit test

--- spec/unit/user_spec.rb
+++ spec/unit/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, :type => :model do
 
-  it "is valid with values given for first name and last name" do
+  it "is valid with values given for first name & last name" do
     user = User.new(first_name: "anything", last_name: "anything")
     expect(user).to be_valid
   end
