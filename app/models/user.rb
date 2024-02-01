48af81771eda33f9a0e4e978081aa262f5573b99
root <root@localhost>
2024-02-01 19:26:51 +0000

    bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
 #  has_many :posts
 #  has_many :comments
-#add another comment
+#add another comment. Yeah
+
 #validates_presence_of :first_name
 #validates_presence_of :last_name
 end
