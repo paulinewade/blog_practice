5e591f8e636957bc0de8db93b04f8383d46441d0
root <root@localhost>
2024-02-01 19:24:59 +0000

    introduced a bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
 #  has_many :comments
 #added validation for test to pass
 #add another comment
-validates_presence_of :first_name
-validates_presence_of :last_name
+#validates_presence_of :first_name
+#validates_presence_of :last_name
 end
