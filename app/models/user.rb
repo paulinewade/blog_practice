abe62f7d34a737bcb8c02ad872e40b6990258bc5
Pauline Wade TAMU <57063863+paulinewade@users.noreply.github.com>
2024-02-04 10:29:49 -0600

    Update user.rb - resolve merge conflict

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,6 @@
 class User < ApplicationRecord
 #  has_many :posts
 #  has_many :comments
-<<<<<<< HEAD
-=======
-#added validation for test to pass
->>>>>>> a80aac5e7bc8af39043f61436e2bd26900593864
 validates_presence_of :first_name
 validates_presence_of :last_name
 end
